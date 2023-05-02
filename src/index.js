e76a5a8114abdb4fc2832bdbf38179e95f7961c5
WHB901 <abdulwhbhussain@gmail.com>
2023-05-03 01:45:08 +0500

    Add List Structure, Linters, readme.md, webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import "./style.css";
+import './style.css';
 
 const todoArray = [
   {
-    description: "Get out of bed",
+    description: 'Get out of bed',
     completed: false,
     index: 0,
   },
   {
-    description: "Brush teeth",
+    description: 'Brush teeth',
     completed: false,
     index: 1,
   },
   {
-    description: "Eat breakfast",
+    description: 'Eat breakfast',
     completed: false,
     index: 2,
   },
 ];
 
 todoArray.forEach((todo) => {
-  const todoListElement = document.createElement("span");
-  todoListElement.classList.add("todo-element-padding");
+  const todoListElement = document.createElement('span');
+  todoListElement.classList.add('todo-element-padding');
   todoListElement.innerText = todo.description;
 
-  document.getElementById("todo-list-element").appendChild(todoListElement);
+  document.getElementById('todo-list-element').appendChild(todoListElement);
   document
-    .getElementById("todo-list-element")
-    .appendChild(document.createElement("hr"));
+    .getElementById('todo-list-element')
+    .appendChild(document.createElement('hr'));
 });
