c24a7948381a3c5b5f567a2034009a823dc5e1f8
WHB901 <abdulwhbhussain@gmail.com>
2023-05-03 01:39:35 +0500

    Add List Structure, Linters, readme.md, webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import _ from "lodash";
 import "./style.css";
 
-function component() {
-  const element = document.createElement("div");
+const todoArray = [
+  {
+    description: "Get out of bed",
+    completed: false,
+    index: 0,
+  },
+  {
+    description: "Brush teeth",
+    completed: false,
+    index: 1,
+  },
+  {
+    description: "Eat breakfast",
+    completed: false,
+    index: 2,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+todoArray.forEach((todo) => {
+  const todoListElement = document.createElement("span");
+  todoListElement.classList.add("todo-element-padding");
+  todoListElement.innerText = todo.description;
 
-  return element;
-}
-
-document.body.appendChild(component());
+  document.getElementById("todo-list-element").appendChild(todoListElement);
+  document
+    .getElementById("todo-list-element")
+    .appendChild(document.createElement("hr"));
+});
