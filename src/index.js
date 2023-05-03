7e2af4f49b8df502a10a88bf8a2eeb76e1f2500d
WHB901 <abdulwhbhussain@gmail.com>
2023-05-03 08:04:55 +0500

    Add the Required Changes

--- src/index.js
+++ src/index.js
@@ -11,19 +11,24 @@ const todoArray = [
     completed: false,
     index: 1,
   },
-  {
-    description: 'Eat breakfast',
-    completed: false,
-    index: 2,
-  },
 ];
 
 todoArray.forEach((todo) => {
+  const todoListCheck = document.createElement('input');
+  todoListCheck.setAttribute('type', 'checkbox');
+  todoListCheck.setAttribute('id', `todo-list-check-${todo.index}`);
+  todoListCheck.classList.add('todo-element-padding');
+  document.getElementById('todo-list-element').appendChild(todoListCheck);
+
   const todoListElement = document.createElement('span');
   todoListElement.classList.add('todo-element-padding');
   todoListElement.innerText = todo.description;
 
   document.getElementById('todo-list-element').appendChild(todoListElement);
+  const threeDotIcon = document.createElement('span');
+  threeDotIcon.innerHTML = '&#8942;';
+  document.getElementById('todo-list-element').appendChild(threeDotIcon);
+
   document
     .getElementById('todo-list-element')
     .appendChild(document.createElement('hr'));
